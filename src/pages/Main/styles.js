c3cf97258522235be0989b3e3eb7684ac9669744
ElioenaiFerrari <elioenaiferrari@gmail.com>
2020-01-09 23:51:46 -0300

    estrutura

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import {mainColor} from '~/styles';
+
+export const Container = styled.View`
+  flex: 1;
+  background: ${mainColor};
+`;
---
+++ src/styles.js
@@ -0,0 +1,1 @@
+export const mainColor = '#6F2682';
